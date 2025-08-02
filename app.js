917284379fedb21b348f6ca988cba3f20c567321
vijay Thakur <practice66375@gmail.com>
2025-08-02 17:01:21 +0530

    apcfe

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const corsOptions = {
     'http://localhost:3001',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5500',
-    'https://adminsite-1-1247.onrender.com',
+   'https://adminsite-1-l247.onrender.com',
     'https://fronter.netlify.app'
   ],
   credentials: true,
